d2dd0d05ab4af61b575879f237962da1f12e60a4
Tanvir Basi <>
2023-09-23 17:46:02 +0100

    Finished website, deployed to firebase

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,7 +4,7 @@ function Resume() {
     return (
         <div>
             <h2 className='text-4xl md:text-4xl mb-1 md:mb-3 font-bold'>Resume</h2>
-            <embed src="src/assets/cv-2.0.pdf#toolbar=0&navpanes=0&scrollbar=0" type="application/pdf" width="100%" height="1140px" />
+            <embed src="assets/cv-2.0.pdf#toolbar=0&navpanes=0&scrollbar=0" type="application/pdf" width="100%" height="1140px" />
 
         </div>
     )
